5af8fdd5e66b75f75711c7e6556e5caedca7596a
Senoir-Shinobi <59853846+Senior-Shinobi@users.noreply.github.com>
2022-11-18 19:03:08 -0500

    added tempManager and the thermometer driver
    some base code from previous assignments.

--- src/RARS-COMP4900.c
+++ src/RARS-COMP4900.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <spawn.h>
+#include <sys/neutrino.h>
+#include <unistd.h>
 
 int main(void) {
 	puts("Hello World!!!"); /* prints Hello World!!! */
+
+	//arguments for spawned children
+	char                *args[] = {};
+	//int                 status;
+	pid_t               pid;
+	struct inheritance  inherit;
+	inherit.flags = 0;
+
+	if ((pid = spawn("TempManager", 0, NULL, &inherit, args, environ)) == -1)
+		    perror("spawn() failed");
+
+
+
 	return EXIT_SUCCESS;
 }
---
+++ src/TempManager.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/neutrino.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+	printf("TempManager\n");
+
+	/*
+	typedef union
+	{
+		struct _pulse pulse;
+	    char rmsg [MAX_STRING_LEN +1];
+	} myMessage_t;
+	myMessage_t msg;
+	*/
+
+	int chid, rcvid;
+	name_attach_t* attach;
+	attach = name_attach(NULL, "TempMan", 0);
+
+	//create a channel
+	chid = ChannelCreate(0);
+
+
+	//spawn thermometer
+
+	//spawn drivers (AC, Heater)
+
+
+	/*while(1){
+
+		rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
+
+	 }*/
+	return EXIT_SUCCESS;
+}
