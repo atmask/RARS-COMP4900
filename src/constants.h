6c3aa4c6895b708b6c3dcdf65ed21b0353d4175a
atmask <benjaminajm@gmail.com>
2022-11-18 20:48:48 -0500

    Add macros/const to constants.h

--- src/constants.h
+++ src/constants.h
@@ -9,5 +9,18 @@
 #define SRC_CONSTANTS_H_
 
 
+/*Message type for interacting with the sensor server*/
+#define GET_DATA 1
+
+
+
+/*Actuator Constants*/
+#define UP 1
+#define DOWN 2
+#define OFF 3
+
+/*SENSOR TYPES*/
+#define TEMPERATURE 1
+
 
 #endif /* SRC_CONSTANTS_H_ */
