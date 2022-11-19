d5bdcb0d75ef9987d86367ad411b3781a797cbb2
atmask <benjaminajm@gmail.com>
2022-11-18 20:45:34 -0500

    Create the outline for hte temp sensor

--- src/temperature_sensor.c
+++ src/temperature_sensor.c
@@ -1,12 +1,29 @@
 /*
  * temperature_sensor.c
  *
+ *	Multi-threaded process that pulls data from the read end of the pipe queue and stores the data point.
+ *	Another thread waits on Client messages for data and returns the data
+ *
  *  Created on: Nov. 18, 2022
  *      Author: benmask
  */
 #include <stdio.h>
 
+void runServer();
+void readData();
+
 int main(void) {
-	printf("Hello from ts");
+	printf("Starting temp sensor");
+	float tempData;
+
+	/*Get the fd from the args passed in*/
+
+	/*Init a mutex for the data*/
+
+	/*Spawn readData thread*/
+
+	/*Spawn the runServer thread*/
+
+	/*wait on the threads*/
 }
 
